d1ceb295f03cb4c7ebe15afe1dc1fd779db55f1c
João Brilhante <joao.brilhante@outlook.com>
2024-01-06 02:17:47 +0100

    Move the Jest config to a dedicated config file.

---
+++ apps/api/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rootDir: "src",
+  testEnvironment: "node",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  moduleFileExtensions: ["ts", "js", "json"],
+};
---
+++ apps/api/test/jest-e2e.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rootDir: ".",
+  testEnvironment: "node",
+  testRegex: ".e2e-spec.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "js", "json"],
+};
