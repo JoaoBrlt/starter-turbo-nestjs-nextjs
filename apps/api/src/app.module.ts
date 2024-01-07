a0380ea4a3ae92d04a7483b365548ad1cfec17c3
João Brilhante <joao.brilhante@outlook.com>
2024-01-07 21:34:28 +0100

    Connect the NestJS app to the database.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import * as process from "process";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DATABASE_HOST || "localhost",
+      port: parseInt(process.env.DATABASE_PORT || "5432", 10),
+      username: process.env.DATABASE_USER || "user",
+      password: process.env.DATABASE_PASSWORD || "password",
+      database: process.env.DATABASE_NAME || "database",
+      entities: ["dist/**/*.entity.{js,ts}"],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
