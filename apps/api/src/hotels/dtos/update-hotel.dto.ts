1a300d4fcc49092f3663d03a975da153579fd053
João Brilhante <joao.brilhante@outlook.com>
2024-01-08 00:23:46 +0100

    Document the API of the NestJS app using Swagger.

--- apps/api/src/hotels/dtos/update-hotel.dto.ts
+++ apps/api/src/hotels/dtos/update-hotel.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from "@nestjs/mapped-types";
 import { CreateHotelDto } from "./create-hotel.dto";
+import { PartialType } from "@nestjs/swagger";
 
 export class UpdateHotelDto extends PartialType(CreateHotelDto) {}
