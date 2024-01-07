a961ea8636972b15506de1fbdc15940b84467875
João Brilhante <joao.brilhante@outlook.com>
2024-01-08 00:03:29 +0100

    Validate incoming requests of the NestJS app.

--- apps/api/src/hotels/dtos/create-hotel.dto.ts
+++ apps/api/src/hotels/dtos/create-hotel.dto.ts
@@ -1,7 +1,24 @@
+import { IsNotEmpty, IsNumber, IsString, Min } from "class-validator";
+
 export class CreateHotelDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsNumber()
+  @Min(0)
+  @Min(5)
   stars: number;
+
+  @IsString()
+  @IsNotEmpty()
   address: string;
+
+  @IsString()
+  @IsNotEmpty()
   city: string;
+
+  @IsString()
+  @IsNotEmpty()
   country: string;
 }
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuration
   const configService = app.get(ConfigService);
   const port = configService.get<number>("port") as number;
+
+  // Validation
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
 }
